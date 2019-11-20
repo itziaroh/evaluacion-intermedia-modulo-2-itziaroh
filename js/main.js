c8240beb033385dde1ffc1090506c104becd9d7e
Itziar Otega <itziarortega94@gmail.com>
2019-11-20 16:24:57 +0100

    Make attemps count function

--- js/main.js
+++ js/main.js
@@ -5,7 +5,41 @@ const tryButton = document.querySelector('#button');
 const clueText = document.querySelector('#clue-text');
 const attemptText = document.querySelector('#attempt-text');
 
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+const getRandomNumber = () => {
+    return Math.ceil(Math.random() * 100);
 }
-console.log(getRandomNumber(100));
+console.log(getRandomNumber());
+
+function selectedNumber() {
+    console.log(numberInput.value);
+}
+
+function checkSelectedNumber() {
+    if (parseInt(numberInput.value) === getRandomNumber()) {
+        clueText.innerHTML = 'Has ganado campeona!!!';
+    }
+    if (parseInt(numberInput.value) < getRandomNumber()) {
+        clueText.innerHTML = 'Demasiado bajo';
+    }
+    if (getRandomNumber() < parseInt(numberInput.value)) {
+        clueText.innerHTML = 'Demasiado alto';
+    }
+    if (parseInt(numberInput.value < 1 || parseInt(numberInput.value) > 100)) {
+        clueText.innerHTML = 'El número debe estar entre 1 y 100';
+    }
+}
+
+let clickCount = 0;
+function clickCountAdd() {
+    clickCount += 1;
+    attemptText.innerHTML = `Numero de intentos: ${clickCount}`;
+}
+
+function pressButton() {
+    selectedNumber();
+    checkSelectedNumber();
+    clickCountAdd();
+}
+
+
+tryButton.addEventListener('click', pressButton);
